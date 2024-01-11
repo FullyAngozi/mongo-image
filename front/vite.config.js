34d3b0768586fa15fac2a0d022d79ae98f09b007
FullyAngozi <ochuks33@yahoo.com>
2024-01-11 16:36:22 -0500

    no

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://harmonious-selkie-a29df1.netlify.app/',
 })
